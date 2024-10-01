5a386a17b17cbd77e19aa02f9219b4849f2a367a
ThiagoSantos7 <t.roberto.santos2024@gmail.com>
2024-10-01 08:45:33 -0300

    Meu commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const caixaDeTexto = document.getElementById("pesquisar")
+const header = document.querySelector(".home-container")
+const carrinho = document.querySelector(".carrinho")
+
+function aparecerInput(){
+    caixaDeTexto.style.visibility = "visible"
+} 
+function sumirInput(){
+    caixaDeTexto.style.visibility = "hidden"
+}
+
+function aparecerCarrinho(){
+    carrinho.style.visibility = "visible"
+}
+
+function sumirCarrinho(){
+    carrinho.style.visibility = "hidden"
+}
+
+
+
+
+
